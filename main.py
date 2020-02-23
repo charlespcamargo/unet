078fab52700e25fe4558bcd583c33150f6112975
Charles Camargo <charles@sltlabs.com.br>
2020-02-23 11:50:37 -0300

    alterações

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import sys
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
+
+# datetime object containing current date and time
+now = datetime.now()  
+dt_string = now.strftime("%Y.%m.%d_%H%M%S")
+
+
 if (True):
     data_gen_args = dict(rotation_range=0.2,
                          width_shift_range=0.05,
@@ -17,22 +23,21 @@ if (True):
                          horizontal_flip=True,
                          fill_mode='nearest')
 
+    pathImg = 'img/' + dt_string 
+    Path(pathImg).mkdir(parents=True, exist_ok=True) 
     
-    myGene = trainGenerator(1, 'data/train', 'image', 'label', data_gen_args, target_size=(1024, 1024))
+    myGene = trainGenerator(10, 'data/train', 'image', 'label', data_gen_args, target_size=(256, 256),save_to_dir=None)
 
     model = unet(pretrained_weights=None, input_size=(256, 256, 1))
     model_checkpoint = ModelCheckpoint('unet_hdf5', monitor='loss', verbose=1, save_best_only=True)
-    model.fit_generator(myGene, steps_per_epoch=1, epochs=1, callbacks=[model_checkpoint])  
+    model.fit_generator(myGene, steps_per_epoch=50, epochs=10, callbacks=[model_checkpoint])  
     
 else:    
-    model = unet(pretrained_weights='unet_hdf5', input_size=(1024, 1024, 1))
+    model = unet(pretrained_weights='unet_hdf5', input_size=(256, 256, 1))
     model_checkpoint = ModelCheckpoint('unet_hdf5', monitor='loss', verbose=1, save_best_only=True)
-    
-    # datetime object containing current date and time
-    now = datetime.now()  
-    dt_string = now.strftime("%Y.%m.%d_%H%M%S")
-    path = "data/test/predict/" + dt_string
 
+
+    path = "data/test/predict/" + dt_string 
     Path(path).mkdir(parents=True, exist_ok=True) 
 
     testGene = testGenerator("data/test", target_size=(256, 256))
