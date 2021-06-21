3bb2cf6a4097bf2c5f9ad783e33ad16fbb86b2f2
Charles Camargo <charles@sltlabs.com.br>
2021-06-21 17:12:20 -0300

    ajustes

--- unet_helper.py
+++ unet_helper.py
@@ -57,6 +57,7 @@ class UnetHelper:
     use_numpy = False
     learning_rate = 1e-4
     momentum = 0.90
+    use_sgd = False
 
     def main(self, args):
 
@@ -113,6 +114,7 @@ class UnetHelper:
         print("use_numpy: ", self.use_numpy)
         print("learning_rate: ", self.learning_rate)
         print("momentum:", self.momentum)
+        print("use_sgd:", self.use_sgd)
 
 
     def set_arguments(
@@ -134,7 +136,8 @@ class UnetHelper:
         validation_steps=200,
         use_numpy = False,
         learning_rate = 1e-4,
-        momentum = 0.90
+        momentum = 0.90,
+        use_sgd = False
     ):
         self.batch_size = batch_size
         self.steps_per_epoch = steps_per_epoch
@@ -159,6 +162,7 @@ class UnetHelper:
         self.use_numpy = use_numpy
         self.learning_rate = learning_rate
         self.momentum = momentum
+        self.use_sgd = use_sgd
 
     def get_folder_name(self, basePath):
         now = datetime.now()
@@ -383,11 +387,11 @@ class UnetHelper:
             )
             raise e 
 
-    def get_model(self, pretrained_weights = None, cnn_type = 0, use_sgd = False):
+    def get_model(self, pretrained_weights = None, cnn_type = 0):
         unet = Unet()
 
         if(cnn_type == 0):
-            return unet.create_model(pretrained_weights=pretrained_weights, input_size=self.input_shape, num_class=2, learning_rate = self.learning_rate, momentum = self.momentum, use_sgd = use_sgd)
+            return unet.create_model(pretrained_weights=pretrained_weights, input_size=self.input_shape, num_class=2, learning_rate = self.learning_rate, momentum = self.momentum, use_sgd = self.use_sgd)
         elif(cnn_type == 1):
             return unet.create_model_keras(img_size=self.input_shape, num_classes=2)        
         else:
