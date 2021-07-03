e103d08ae3daa66a9898fa8b1e339b1355d40f1e
Charles Camargo <charles@sltlabs.com.br>
2021-07-03 17:39:09 -0300

    adicionando o calculo de cobertura do lirio/cm

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ from skimage.util import img_as_uint
 from pathlib import Path
 import cv2
 from sklearn.utils import class_weight
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw, ImageFont
 from tensorflow.python.keras.preprocessing.image import DirectoryIterator
 from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
@@ -228,78 +228,124 @@ class Data():
         
         save_path = base_path + "diff/"    
         Path(save_path).mkdir(parents=True, exist_ok=True)
+        font = ImageFont.truetype("assets/arial-unicode.ttf", 18)
+        gsd = 1.73 # 1.73cm/px
 
-        for i, file_name in enumerate(imgs):
-            
-            images = Data.load_images_to_compare(base_path, file_name)
+        for i, file_name in enumerate(imgs):            
+            image_data = Data.load_images_to_compare(base_path, file_name)
+            images = image_data[0]
+            images_masks_size = image_data[1]
             border = columns = 4
+            subtitle_size = 20
             rows = 2
-            total_width, max_height, border, img_mais_larga, img_mais_alta = Data.get_sizes(images, border, columns, rows)
+            total_width, max_height, border, img_mais_larga, img_mais_alta = Data.get_sizes(images, border, columns, rows, subtitle_size)
             
             composed_img = Image.new('RGBA', (total_width, max_height), color="gray")
             x_offset = border
-            y_offset = border
+            y_offset = border + subtitle_size
             total_imgs = (len(images) if rows * columns  > len(images)  else rows * columns) 
             current_row = 0
-            
+            current_column = 0
+
             for j in range(0, total_imgs):
+                current_column += 1
                 img = images[j]
-                composed_img.paste(img, (x_offset, y_offset))
+                mask_size = images_masks_size[j]
+                Data.add_image(composed_img, img, current_column, x_offset, y_offset, subtitle_size, border, font, mask_size, gsd)
                 x_offset += (img_mais_larga + border)
 
-                if(j == columns - 1):
+                if(current_column == columns):
+                    current_column = 0
                     current_row += 1
                     x_offset = border
-                    y_offset = border * (current_row + 1) + img_mais_alta
+                    y_offset = (subtitle_size + border) * (current_row + 1) + img_mais_alta                
 
             composed_img.save(os.path.join(save_path, imgs[i] + ".png"))
 
-            if(i >= 5):
-                break
+            # if(i >= 5):
+            #     break
 
     @staticmethod
     def load_images_to_compare(base_path, file_name):
+        
         original = io.imread(os.path.join(base_path, "images", file_name), as_gray=False)
         mask = io.imread(os.path.join(base_path, "masks", file_name), as_gray=False)
         predict = io.imread(os.path.join(base_path, "results", file_name + Data.predict_suffix), as_gray=False)
-
+        
         mask[mask >= 128] = 255
         mask[mask < 128] = 0
         mask_predict_diff = ((np.abs(mask[:,:,0] - predict) > 0.5) * 255).astype(np.uint8)
-
+        
+        original_rgba = Image.fromarray(original).convert("RGBA")
+        original_filter_mask, mask_pixels = Data.parse_mask_to_rgba(mask, 0)
+        original_filter_predict, predict_mask_pixels = Data.parse_mask_to_rgba(predict, 1)
+        original_filter_diff, diff_mask_pixels = Data.parse_mask_to_rgba(mask_predict_diff, 2)
         original = Image.fromarray(original)
-        original_rgba = original.convert("RGBA")
-        original_filter_mask = Image.alpha_composite(original_rgba, Data.parse_mask_to_rgba(mask, 0))
-        original_filter_predict = Image.alpha_composite(original_rgba, Data.parse_mask_to_rgba(predict, 1))
-        original_filter_diff = Image.alpha_composite(original_rgba, Data.parse_mask_to_rgba(mask_predict_diff, 2))
 
-        mask = Image.fromarray(mask).convert('RGB')
-        predict = Image.fromarray(predict).convert('RGB')
-        mask_predict_diff = Image.fromarray(mask_predict_diff).convert('RGB')
-
-        images = [
-                    original,
-                    mask,
-                    predict,
-                    mask_predict_diff,
-
-                    original,
-                    original_filter_mask,
-                    original_filter_predict,
-                    original_filter_diff,
+        image_data = [
+                        [
+                            original,
+                            Image.fromarray(mask).convert('RGB'),
+                            Image.fromarray(predict).convert('RGB'),
+                            Image.fromarray(mask_predict_diff).convert('RGB'),
+                            original,
+                            Image.alpha_composite(original_rgba, original_filter_mask),
+                            Image.alpha_composite(original_rgba, original_filter_predict),
+                            Image.alpha_composite(original_rgba, original_filter_diff),
+                        ],
+                        [
+                            0,
+                            0,
+                            0,
+                            0,
+                            0,
+                            mask_pixels,
+                            predict_mask_pixels,
+                            diff_mask_pixels
+                        ]
                     ]
 
-        return images
+        return image_data
 
     @staticmethod
-    def get_sizes(images, border, columns, rows):
+    def get_sizes(images, border, columns, rows, subtitle_size):
         widths, heights = zip(*(img.size for img in images))
         img_mais_alta = max(widths)
         img_mais_larga = max(heights)
         total_width = (img_mais_alta * columns) + ((columns + 1) * border)
-        max_height = (img_mais_larga * rows) + ((rows + 1) * border)
+        max_height = (img_mais_larga * rows) + ((rows + 1) * (border + subtitle_size))
 
         return total_width, max_height, border, img_mais_larga, img_mais_alta
+
+    @staticmethod
+    def add_image(composed_img, img, column_type, x_offset, y_offset, subtitle_size, border, font, mask_size, gsd):
+        composed_img.paste(img, (x_offset, y_offset))
+        draw = ImageDraw.Draw(composed_img)
+        subtitle_text = Data.get_image_subtitle_text(column_type, mask_size, gsd)
+        draw.text((x_offset, y_offset - subtitle_size - border), subtitle_text, (255, 255, 255), font=font)
+
+    @staticmethod
+    def get_image_subtitle_text(column_type, mask_size, gsd):
+        if(column_type == 1):
+            return f'original{Data.get_size(mask_size, gsd)}'
+        elif (column_type == 2):
+            return f'ground-truth{Data.get_size(mask_size, gsd)}'
+        elif (column_type == 3):
+            return f'predict{Data.get_size(mask_size, gsd)}'
+        else:
+            return f'diff ground-truth and predict{Data.get_size(mask_size, gsd)}'
+
+    @staticmethod
+    def get_size(mask_size, gsd):
+        if(mask_size == 0 or gsd == 0):
+            return ''
+
+        total = mask_size * gsd
+
+        if(total / 100 > 1):
+            return f': {mask_size}px - {round(total / 10000, 2)}m²' 
+        else:
+            return f': {mask_size}px - {round(total, 2)}cm' 
 
     @staticmethod
     def parse_mask_to_rgba(mask, mask_type):
@@ -310,11 +356,11 @@ class Data():
         RGBA = np.dstack((mask, np.zeros(mask.size, dtype = np.uint8) + 255))            
 
         # Make mask of black or white pixels too. - mask is True where image has the right color
-        mask_black = (RGBA[:, :, 0:3] == [0,0,0]).all(2)
-        white_black = (RGBA[:, :, 0:3] == [255,255,255]).all(2)
+        black_mask = (RGBA[:, :, 0:3] == [0,0,0]).all(2)
+        white_mask = (RGBA[:, :, 0:3] == [255,255,255]).all(2)
         
         # Make all pixels matched by mask into transparent ones
-        RGBA[mask_black] = (0,0,0,0)
+        RGBA[black_mask] = (0,0,0,0)
 
         # Make all pixels matched by mask into "filter" purple color with transparency
         alpha_percent = 50
@@ -329,6 +375,6 @@ class Data():
             alpha = int(alpha_percent/100*255)
             color = (255, 0, 0, alpha)  # red
 
-        RGBA[white_black] = color
+        RGBA[white_mask] = color
 
-        return Image.fromarray(RGBA)
+        return Image.fromarray(RGBA), np.sum(white_mask)
