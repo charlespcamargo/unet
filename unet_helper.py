65e6cea2aef3990d904cb828bebbb7622bed55a9
Charles Camargo <charles@sltlabs.com.br>
2021-07-20 11:18:38 -0300

    ajustes

--- unet_helper.py
+++ unet_helper.py
@@ -414,8 +414,8 @@ class UnetHelper:
         
         if(self.use_augmentation):
             data_gen_args = dict(
-                                    zoom_range = [0.00, 0.02],  # alterar
-                                    brightness_range=[0.00, 0.02], # alterar
+                                    zoom_range = [0.01, 0.05],  # alterar
+                                    brightness_range=[0.01, 0.05], # alterar
                                     shear_range = 0.05,
                                     horizontal_flip=True, # remover
                                     vertical_flip=True, # remover            
